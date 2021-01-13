ed04c669378c58e35b2a94b8a961a19d10abecab
YazidIlyasBaihaqi <yazi20012ti@student.nurulfikri.ac.id>
2021-01-13 09:56:56 +0700

    test

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,8 @@
 function classToggle() {
-    const navs = document.querySelectorAll('.navbar-tems')
+    const navs = document.querySelectorAll('.Navbar__Items')
     
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
   }
   
-  document.querySelector('.navbar-toggler')
+  document.querySelector('.Navbar__Link-toggle')
     .addEventListener('click', classToggle);
