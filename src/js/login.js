66605d36a6dcdd0abe200082b62c1b011e25fa4c
YazidIlyasBaihaqi <yazi20012ti@student.nurulfikri.ac.id>
2021-01-22 10:05:03 +0700

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -4,6 +4,6 @@ function loginAuth(){
     const login = "admin";
     const password = "admin";
     if (InpLogin === login && InpPassword === password)
-        window.location.replace("https://yazidilyasbaihaqi.github.io/livegithubtest/adminindex.html");
+        window.location.replace("https://yazidilyasbaihaqi.github.io/Kelompok2Live/adminindex.html");
     else console.log("Login gagal")
 }
